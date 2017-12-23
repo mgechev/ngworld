f7788622765b8ead4b98d184853bb8c0f22017fb
mgechev <mgechev@gmail.com>
2017-12-23 18:36:45 +0200

    style: update logging

--- parser/create-program.ts
+++ parser/create-program.ts
@@ -9,7 +9,7 @@ export const normalizeOptions = (options: any, configFilePath: string) => {
 };
 
 export const createProgramFromTsConfig = (configFile: string, overrideFiles: string[] = undefined): ts.Program => {
-  console.log(cyan('📝\t', 'Creating a program...'));
+  console.log(cyan('Creating a program...'));
   const projectDirectory = dirname(configFile);
   const { config } = ts.readConfigFile(configFile, ts.sys.readFile);
 
@@ -26,7 +26,7 @@ export const createProgramFromTsConfig = (configFile: string, overrideFiles: str
   const host = ts.createCompilerHost(parsed.options, true);
   const program = ts.createProgram(overrideFiles || parsed.fileNames, parsed.options, host);
 
-  console.log(green('✅\t', 'Program created!'));
+  console.log(green('Program created!'));
 
   return program;
 };
